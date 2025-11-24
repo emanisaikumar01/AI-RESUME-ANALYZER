88ecf5d4eeaaa33683c4c95e1fc816f46321601b
emanisaikumar01 <emanisaikumar01@gmail.com>
2025-11-24 23:18:48 +0530

    Update uploadRoute.js

--- backend/src/routes/uploadRoute.js
+++ backend/src/routes/uploadRoute.js
@@ -22,8 +22,8 @@ router.post("/", upload.single("file"), async (req, res) => {
 - ATS score
 \n\n${extractedText}`;
 
-   const response = await fetch(
-  `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+  const response = await fetch(
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
   {
     method: "POST",
     headers: { "Content-Type": "application/json" },
