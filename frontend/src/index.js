eb8db2761bd4c62279f16527d94eb57ba4dff16d
emanisaikumar01 <emanisaikumar01@gmail.com>
2026-01-21 07:48:57 +0530

    Import UploadStyle.css in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
 import "./styles/app.css";
 import "./styles/neon.css";
 import "./styles/dashboard.css";
+import "./styles/UploadStyle.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <BrowserRouter>
     <App />
